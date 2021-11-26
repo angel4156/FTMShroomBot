2dd0381f904199e7d885bbe90d7faf9ce6aa9f5d
angel4156 <angel4156@hotmail.com>
2021-11-26 07:18:02 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,3 @@
 const crypto = require("./modules/crypto.js")
 
-console.log("Encrypt", crypto.encrypt("OTEzNTIyMDcyOTQ3NDA4ODk3.YZ_tug.m6Sg-ZtRh4-RCLztPVQkQ_J1sEE"))
+console.log("Encrypt", crypto.encrypt(""))
